3d67aa186eb7605f3369273da8666b8fb287e303
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-05 22:44:46 +0530

    annual investment property added into the form

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,13 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
+  indianCurrencySign = 'INR';
 }
