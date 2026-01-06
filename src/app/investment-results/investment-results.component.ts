ea94e4b56a290252cf086e3ef3fb701394c062b6
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-06 22:36:06 +0530

    Using of signals in ts files

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, Input } from '@angular/core';
+import { Component, computed, inject, input, Input } from '@angular/core';
 import { InvestmentResultInput } from './investment-results.model';
 import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from '../investment.service';
@@ -13,7 +13,5 @@ import { InvestmentService } from '../investment.service';
 export class InvestmentResultsComponent {
   private investmentService = inject(InvestmentService);
 
-  get results(){
-    return this.investmentService.resultsData();
-  }
+  results = computed(() => this.investmentService.resultsData());
 }
