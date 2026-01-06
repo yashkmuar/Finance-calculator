4cfd1bfc5077bc10c1972c0d5841e9ae7e630af8
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-06 22:05:41 +0530

    uses of constructor to invoke the investmentService object and assign values of properties accordingly

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,6 +1,32 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
+import { InvestmentInput } from "./user-input/user-input.model";
+import { InvestmentResultInput } from "./investment-results/investment-results.model";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class InvestmentService {
+    resultsData = signal<InvestmentResultInput[] | undefined>(undefined);
 
+    calculateInvestmentResults(data: InvestmentInput) {
+        const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
+        const annualData = [];
+        let investmentValue = initialInvestment;
+
+        for (let i = 0; i < duration; i++) {
+            const year = i + 1;
+            const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+            investmentValue += interestEarnedInYear + annualInvestment;
+            const totalInterest =
+                investmentValue - annualInvestment * year - initialInvestment;
+            annualData.push({
+                year: year,
+                interest: interestEarnedInYear,
+                valueEndOfYear: investmentValue,
+                annualInvestment: annualInvestment,
+                totalInterest: totalInterest,
+                totalAmountInvested: initialInvestment + annualInvestment * year,
+            });
+        }
+
+        this.resultsData?.set(annualData);
+    }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -2,6 +2,7 @@ import { CurrencyPipe } from '@angular/common';
 import { Component, EventEmitter, output, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import type { InvestmentInput } from './user-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -11,15 +12,19 @@ import type { InvestmentInput } from './user-input.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
 
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
   enteredExpectedReturn = signal('5');
   enteredDuration = signal('10');
 
+  constructor(private investmentService: InvestmentService) {
+
+  }
+
+
   onSubmit(){
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(),
       annualInvestment: +this.enteredAnnualInvestment(),
       expectedReturn: +this.enteredExpectedReturn(),
