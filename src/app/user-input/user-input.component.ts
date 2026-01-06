bf59ca9e592a75d61ac2774b5de57a4fdfa118c6
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-06 09:53:59 +0530

    FormsModule imported so that ngSubmit works when form submitted, testing also done through console.log

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,13 +1,17 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
   indianCurrencySign = 'INR';
+  onSubmit(){
+  console.log("Form submitted");
+  }
 }
