38f96970511b2ca44e0c158b4ad8138271a1988a
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-06 22:13:15 +0530

    signals syntax updated in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,27 +14,5 @@ import { InvestmentResultInput } from './investment-results/investment-results.m
 export class AppComponent {
   resultsData = signal<InvestmentResultInput[] | undefined>(undefined);
 
-  oncalculateInvestmentResults(data: InvestmentInput) {
-  const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
-  const annualData = [];
-  let investmentValue = initialInvestment;
-
-  for (let i = 0; i < duration; i++) {
-    const year = i + 1;
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    const totalInterest =
-      investmentValue - annualInvestment * year - initialInvestment;
-    annualData.push({
-      year: year,
-      interest: interestEarnedInYear,
-      valueEndOfYear: investmentValue,
-      annualInvestment: annualInvestment,
-      totalInterest: totalInterest,
-      totalAmountInvested: initialInvestment + annualInvestment * year,
-    });
-  }
-
-  this.resultsData?.set(annualData);
-}
+  
 }
