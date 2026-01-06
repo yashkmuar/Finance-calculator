46bed41d1dde7b364929a271969ec76c58a616ad
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-06 10:14:36 +0530

    back logic copying into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,30 @@ import { UserInputComponent } from './user-input/user-input.component';
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+
+
+  calculateInvestmentResults(data: {initialInvestment:number, annualInvestment:number, expectedReturn:number, duration:number}) {
+  const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
+  const annualData = [];
+  let investmentValue = initialInvestment;
+
+  for (let i = 0; i < duration; i++) {
+    const year = i + 1;
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+    investmentValue += interestEarnedInYear + annualInvestment;
+    const totalInterest =
+      investmentValue - annualInvestment * year - initialInvestment;
+    annualData.push({
+      year: year,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: annualInvestment,
+      totalInterest: totalInterest,
+      totalAmountInvested: initialInvestment + annualInvestment * year,
+    });
+  }
+
+  return annualData;
+}
+}
