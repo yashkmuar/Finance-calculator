1ec8cf7f4f1e0b9f0aad0ac04de155bc4c8984da
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-06 17:41:16 +0530

    now results data stored in array object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import type { InvestmentInput } from './user-input/user-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+import { InvestmentResultInput } from './investment-results/investment-results.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent]
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
 export class AppComponent {
-
+  resultsData?: InvestmentResultInput[];
 
   oncalculateInvestmentResults(data: InvestmentInput) {
   const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
